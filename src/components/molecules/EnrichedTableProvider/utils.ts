ef04f8f5cb1f46ee0b2f4f2538686c4b85438fd2
typescreep <typescri@yandex.ru>
2025-06-10 17:42:09 +0300

    table keys fix

--- src/components/molecules/EnrichedTableProvider/utils.ts
+++ src/components/molecules/EnrichedTableProvider/utils.ts
@@ -1,6 +1,6 @@
 import { TableProps } from 'antd'
 import jp from 'jsonpath'
-import { v4 as uuidv4 } from 'uuid'
+// import { v4 as uuidv4 } from 'uuid'
 import { TJSON } from 'localTypes/JSON'
 import { TAdditionalPrinterColumns } from 'localTypes/richTable'
 
@@ -39,7 +39,8 @@ export const prepareDataForEnrichedColumns = ({
       let newDataIndex: string | undefined = ''
 
       if (jsonPath.includes('[')) {
-        newDataIndex = uuidv4()
+        // newDataIndex = uuidv4()
+        newDataIndex = JSON.stringify(jsonPath)
         customFields.push({ dataIndex: newDataIndex, jsonPath })
       }
 
