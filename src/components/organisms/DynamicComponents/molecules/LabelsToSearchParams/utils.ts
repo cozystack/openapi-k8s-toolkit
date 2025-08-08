85c806e116b08d71da7e635a5e1f9b1e24b38e30
typescreep <typescreep@prorobotech.ru>
2025-08-08 16:26:01 +0300

    linters

--- src/components/organisms/DynamicComponents/molecules/LabelsToSearchParams/utils.ts
+++ src/components/organisms/DynamicComponents/molecules/LabelsToSearchParams/utils.ts
@@ -1,28 +1,17 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-const isRecordStringOrNumber = (value: unknown): value is Record<string, string | number> => {
-  if (typeof value !== 'object' || value === null || Array.isArray(value)) {
-    return false
-  }
-
-  for (const key in value) {
-    if (typeof key !== 'string') return false
-
-    const val = (value as Record<string, unknown>)[key]
-    if (typeof val !== 'string' && typeof val !== 'number') {
-      return false
-    }
-  }
-
-  return true
-}
+const isRecordStringNumber = (value: unknown): value is Record<string, string | number> =>
+  typeof value === 'object' &&
+  value !== null &&
+  !Array.isArray(value) &&
+  Object.entries(value).every(([k, v]) => typeof k === 'string' && (typeof v === 'string' || typeof v === 'number'))
 
 export const parseArrayOfAny = (value: any[]): { data?: Record<string, string | number>; error?: string } => {
   if (!Array.isArray(value)) {
     return { error: 'Value on jsonPath is not an array' }
   }
 
-  if (isRecordStringOrNumber(value[0])) {
+  if (isRecordStringNumber(value[0])) {
     return { data: value[0] }
   }
 
