4ddd56e6ddc4aeaaad7f5bb98d89bcd851621ae9
typescreep <typescreep@prorobotech.ru>
2025-11-01 23:03:32 +0300

    small improvements

--- src/components/molecules/BlackholeForm/molecules/FormStringInput/FormStringInput.tsx
+++ src/components/molecules/BlackholeForm/molecules/FormStringInput/FormStringInput.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-nested-ternary */
-import React, { FC, useState, useEffect } from 'react'
+import React, { FC, useMemo } from 'react'
 import { Flex, Input, Typography, Tooltip, Button, Form } from 'antd'
 import { getStringByName } from 'utils/getStringByName'
 import { isMultilineString } from 'utils/isMultilineString'
@@ -36,31 +36,13 @@ export const FormStringInput: FC<TFormStringInputProps> = ({
   onRemoveByMinus,
 }) => {
   const designNewLayout = useDesignNewLayout()
-  const [isMultiline, setIsMultiline] = useState(false)
-  const [currentValue, setCurrentValue] = useState<string>('')
 
   const fixedName = name === 'nodeName' ? 'nodeNameBecauseOfSuddenBug' : name
   const formFieldName = arrName || fixedName
-
-  // Watch the form field value
   const formValue = Form.useWatch(formFieldName)
 
-  // Initialize multiline state based on form value
-  useEffect(() => {
-    if (formValue && typeof formValue === 'string') {
-      setCurrentValue(formValue)
-      if (isMultilineString(formValue)) {
-        setIsMultiline(true)
-      }
-    }
-  }, [formValue])
-
-  // Check if the current value should be multiline
-  useEffect(() => {
-    if (currentValue && isMultilineString(currentValue)) {
-      setIsMultiline(true)
-    }
-  }, [currentValue])
+  // Derive multiline based on current local value
+  const isMultiline = useMemo(() => isMultilineString(formValue), [formValue])
 
   const title = (
     <>
@@ -100,38 +82,6 @@ export const FormStringInput: FC<TFormStringInputProps> = ({
           placeholder={getStringByName(name)}
           rows={isMultiline ? 4 : 1}
           autoSize={!isMultiline ? { minRows: 1, maxRows: 1 } : { minRows: 2, maxRows: 10 }}
-          onChange={e => {
-            const { value } = e.target
-            setCurrentValue(value)
-          }}
-          onInput={e => {
-            // Handle input changes and check for newlines
-            const { value } = e.target as HTMLTextAreaElement
-            setCurrentValue(value)
-
-            // If we detect a newline and we're in single-line mode, switch to multiline
-            if (!isMultiline && value.includes('\n')) {
-              setIsMultiline(true)
-            }
-          }}
-          onBlur={e => {
-            // If the value becomes single line, switch back to single-line mode
-            if (isMultilineString(e.target.value)) {
-              setIsMultiline(true)
-            } else {
-              setIsMultiline(false)
-            }
-          }}
-          onPaste={e => {
-            // Handle paste of multiline content
-            const pastedText = e.clipboardData.getData('text')
-            if (pastedText && isMultilineString(pastedText)) {
-              // Let the default paste behavior happen, but ensure multiline mode
-              setTimeout(() => {
-                setIsMultiline(true)
-              }, 0)
-            }
-          }}
         />
       </ResetedFormItem>
     </HiddenContainer>
